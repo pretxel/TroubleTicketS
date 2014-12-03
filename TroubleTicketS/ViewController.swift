258ff4264f4b72a57ab75c7bfcf6d8801aebccbb
Edsel Serrano <pretxel@hotmail.com>
2014-12-03 11:08:22 -0600

    init

---
+++ TroubleTicketS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TroubleTicketS
+//
+//  Created by Edsel Serrano on 02/12/14.
+//  Copyright (c) 2014 Edsel Serrano. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
